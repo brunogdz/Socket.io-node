a8f0643cd114176810eafab66d42333db343b4d3
Bruno Gomes <brunog.vix@gmail.com>
2021-09-29 19:21:32 -0300

    instalado o cors

--- back-end/README.md
+++ back-end/README.md
@@ -11,5 +11,15 @@ npm i express
 ### 
 npm install -g nodemon
 
-### 
+### Está como dev para não subir quando for para produção
 npm install --save-dev nodemon
+
+### Instalando o socket.io
+npm install socket.io
+
+### Permitir o acesso à API
+npm install cors
+
+## Execuções
+
+nodemon app.js
--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,19 @@
-const express = require('express')
+const express = require('express');
+const socket = require('socket.io');
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Aoba')
 })
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+});
+// server, e as rotas {}
+io = socket(server)
+
+// Assim que fizer uma conexão eu vou ter um id da conexão, cada usuário receber um id
+io.on("connection", (socket) => {
+    console.log(socket.id);
 })
