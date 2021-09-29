408d4bb62a54f4662e0b3b17b149b4909e82a0a4
Bruno Gomes <brunog.vix@gmail.com>
2021-09-29 20:31:15 -0300

    agora implementar o front-end

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const socket = require('socket.io');
+const cors = require('cors');
 const app = express()
 const port = 8080
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type, Authorization");
+    app.use(cors());
+    next();
+})
 
 app.get('/', (req, res) => {
   res.send('Aoba')
@@ -11,7 +20,8 @@ const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 // server, e as rotas {}
-io = socket(server)
+// o cor ao dar o "*" qualquer URL pode fazer conexão
+io = socket(server, {cors: {origin: "*"}})
 
 // Assim que fizer uma conexão eu vou ter um id da conexão, cada usuário receber um id
 io.on("connection", (socket) => {
